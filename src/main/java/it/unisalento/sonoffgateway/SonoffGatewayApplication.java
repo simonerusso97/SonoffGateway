f22eb47724e95b15951c8b5bf49f4a11210cd06e
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-10-11 13:25:04 +0200

    Change status implementata

--- src/main/java/it/unisalento/sonoffgateway/SonoffGatewayApplication.java
+++ src/main/java/it/unisalento/sonoffgateway/SonoffGatewayApplication.java
@@ -1,5 +1,10 @@
 package it.unisalento.sonoffgateway;
 
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +13,6 @@ public class SonoffGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SonoffGatewayApplication.class, args);
-		
-		//TODO: probabilmente una connessione mqtt va qui
 	}
 
 }
--- src/main/java/it/unisalento/sonoffgateway/restController/Controller.java
+++ src/main/java/it/unisalento/sonoffgateway/restController/Controller.java
@@ -1,23 +1,82 @@
 package it.unisalento.sonoffgateway.restController;
 
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
-	@RequestMapping(value="changeStatus", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Boolean> changeStatus(){
+	
+	String broker = "tcp://192.168.1.100:1883";
+	String clientId = "raspberrypi";
+	
+	String cmdTopic = "cmnd/tasmota_8231A8/POWER1";
+	String reqToipic = "cmnd/tasmota_8231A8/Power1";
+	String statTopic = "stat/tasmota_8231A8/POWER1";
+	
+	
+	@RequestMapping(value="changeStatus/{status}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Boolean> changeStatus(@PathVariable("status") String status) throws Exception{
 		try {
-			//TODO: istruzioni al dispositivo sonoff
+			MqttClient client = new MqttClient(broker, clientId, new MemoryPersistence());
+			
+			client.setCallback(new MqttCallback() {
+				
+				@Override
+				public void messageArrived(String topic, MqttMessage message) throws Exception {
+				}
+				
+				@Override
+				public void deliveryComplete(IMqttDeliveryToken token) {
+					System.out.println("PUBLISH SUCCESSFULL");
+				}
+				
+				@Override
+				public void connectionLost(Throwable cause) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+			});	
+			
+			MqttConnectOptions opt = new MqttConnectOptions();
+			
+			opt.setCleanSession(true);
+			
+			System.out.println("CONNECTIONG TO BROKER " + broker);
+			
+			client.connect(opt);
+			
+			System.out.println("CONNECT SUCCESSED");
+			
+			MqttMessage message = new MqttMessage(status.getBytes());
+			
+			client.publish(cmdTopic, message);	//BLOCKING
+
+			client.disconnect();
+			
+			System.out.println("CLIENT DISCONNESSO");
+			
 			return new ResponseEntity<Boolean>(HttpStatus.OK);
+			
 		}catch (Exception e) {
 			throw e;
 			}
-		
 	}
+	
 }
