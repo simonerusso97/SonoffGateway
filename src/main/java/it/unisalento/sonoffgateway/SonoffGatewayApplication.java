22a14cbf7e14148864287cbfeca46a813d279223
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-11-06 20:24:37 +0100

    Cambiata porta http

--- src/main/java/it/unisalento/sonoffgateway/SonoffGatewayApplication.java
+++ src/main/java/it/unisalento/sonoffgateway/SonoffGatewayApplication.java
@@ -1,5 +1,7 @@
 package it.unisalento.sonoffgateway;
 
+import java.util.Collections;
+
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
@@ -8,11 +10,14 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SonoffGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SonoffGatewayApplication.class, args);
-	}
+		SpringApplication app = new SpringApplication(SonoffGatewayApplication.class);
+        app.setDefaultProperties(Collections
+          .singletonMap("server.port", "8081"));
+        app.run(args);	}
 
 }
